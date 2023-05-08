245ec2194f5c1c9a2bd44c7bb53c3712e32d40b9
Tariq Rais <tariq_rais@persistent.com>
2023-05-08 18:03:40 +0530

    pushing codes

---
+++ environments/dev/main.tf
@@ -0,0 +1,20 @@
+ module "vpc" {
+    source = "../../modules/vpc"
+
+    cidr_block        = var.cidr_block
+    #vpc_id            = module.vpc.vpc_id
+    cidr_block_subnet = var.cidr_block_subnet
+    name              = var.name
+    tenancy           = var.tenancy
+  }
+
+  module "ec2" {
+
+    source = "../../modules/ec2"
+
+    ami_id            = var.ami_id
+    instance_type     = var.instance_type
+    region            = var.region
+    subnet_id         = module.vpc.subnet_id
+    name              = var.name
+  }
---
+++ environments/dev/variables.tf
@@ -0,0 +1,20 @@
+# CIDR Block for VPC
+variable "cidr_block" {}
+
+# Subnet CIDR
+variable "cidr_block_subnet" {}
+
+# Instance Tenancy 
+variable "tenancy" {}
+
+# Tags
+variable "name" {}
+
+# Region
+variable "region" {}
+
+# AMI ID
+variable "ami_id" {}
+
+# Instance Type
+variable "instance_type" {}
